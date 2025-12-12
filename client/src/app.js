fef77dfbdc8d86eb67c8c78262e00f9b861c5b03
Andrew Birch <ab8218-cp@yahoo.com>
2025-12-12 13:13:29 +0000

    BUILD: added week 1 assignment styling comments styling reversed comments order showing latest comment at top with while loop

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,7 @@
 console.log("hello world");
 const commForm = document.getElementById("commForm");
+const gbkComm = document.getElementById("guestbook-comments");
+window.addEventListener("load", getData);
 
 function guestBook(e) {
     e.preventDefault();
@@ -11,9 +13,9 @@ function guestBook(e) {
     // we will use the url where our server is located --> http://localhost:8080/comments
     // (make sure you are fetching the specific POST route that will process the form data)
     // we will use fetch to connect our client with the server
-    
+
     fetch("http://localhost:8080/comments", { // TODO: CHANGE TO RENDER 'SERVER URL' WHEN DEPLOYED
-    // fetch("https://week04-assignment-2p2p.onrender.com", {
+        // fetch("https://week04-assignment-2p2p.onrender.com", {
 
         method: "POST",
         headers: {
@@ -29,32 +31,25 @@ commForm.addEventListener("submit", guestBook);
 
 // Get API DATA here
 // Application Programming Interface
-async function getData() { // CREATE ELEMENTS / HANDLERS
+async function getData() { // create 'comments' elements from API object
     // const response = await fetch("https://week04-assignment-2p2p.onrender.com/comments");
     const response = await fetch("http://localhost:8080/comments");
     const json = await response.json();
     console.log("JSON Data:", json); // check data
-    // cpsEl.innerText = `${cps} pps`;
 
-    // for (i = 0; i < json.length; i++) {
-    //     let rewardsElem = document.createElement("button");
-    //     let inc = json[i].increase;
-    //     let cost = json[i].cost;
-    //     let aLevel = anxLevel[i];
-    //     rewardsElem.id = `upgrade${json[i].id}`;
-    //     rewardsElem.disabled = true;
-    //     rewardsElem.title = `buy ${inc} pps for ${cost} panics`;
-    //     rewardsElem.innerHTML = `<span>${aLevel}</span><br>Price - ${cost} Panics<br>Reward - ${inc} pps`;
-    //     shopContainer.appendChild(rewardsElem);
-
-    //     rewardsElem.addEventListener("click", () => { // event handler
-    //         totalCount = totalCount - cost;
-    //         cps = cps + inc;
-    //         stats.totaL = totalCount;
-    //         stats.cPs = cps;
-    //         storeValues("stats", stats);
-    //     });
-    // }
+    let i = json.length-1;
+    while (i >= 0) {
+        let commWrap = document.createElement("div");
+        let commId = json[i].id;
+        let commName = json[i].username;
+        let commText = json[i].comment;
+        commWrap.className = "comment";
+        commWrap.innerHTML = `<h3>${commName}</h3><br><p>${commText}</p>`;
+        gbkComm.appendChild(commWrap);
+        i--;
+    }
 }
 
-getData();
+// todo: clear input fields when form is submitted
+// todo: create link to add a comment and show form?
+// todo: add date to comment
