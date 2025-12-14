456cbab0dae436b1d8a0a961bde39cca9384eb33
Andrew Birch <ab8218-cp@yahoo.com>
2025-12-14 16:20:13 +0000

    FIX: bug fix server js

--- client/src/app.js
+++ client/src/app.js
@@ -37,8 +37,8 @@ function guestBook(e) {
 // Get API DATA here
 // Application Programming Interface
 async function getData() { // create 'comments' elements from API object
-  const response = await fetch("https://week04-assignment-2p2p.onrender.com/comments");
-  // const response = await fetch("https://week04-assignment-2p2p.onrender.com");
+  // const response = await fetch("https://week04-assignment-2p2p.onrender.com/comments");
+  const response = await fetch("https://week04-assignment-2p2p.onrender.com");
   // const response = await fetch("http://localhost:8080/comments");
   const obj = await response.json(); // create JS object
   // console.log("JS object:", obj); // check data
@@ -96,8 +96,8 @@ async function getData() { // create 'comments' elements from API object
       }
 
       // fetch("http://localhost:8080/likes", { // TODO: CHANGE TO RENDER 'SERVER URL' WHEN DEPLOYED
-      fetch("https://week04-assignment-2p2p.onrender.com/likes", {
-      // fetch("https://week04-assignment-2p2p.onrender.com", {
+      // fetch("https://week04-assignment-2p2p.onrender.com/likes", {
+      fetch("https://week04-assignment-2p2p.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
